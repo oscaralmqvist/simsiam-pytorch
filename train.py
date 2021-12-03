4a13677193a0a75c3c6448e3544957b760a93624
Dennis Magnusson <d@a.local>
2021-12-03 17:23:37 +0100

    Add wandb

--- train.py
+++ train.py
@@ -9,26 +9,36 @@ import torch.nn.functional as F
 
 from simsiam import SimSiam
 
+import wandb
+
+import argparse
+import datetime
+
 def get_augmentations(imgsize=64, crop=True, flip=True, jitter=True, grayscale=True, blur=True):
   augs = []
-
+  augs_text = ''
   if crop:
     augs.append(transforms.RandomResizedCrop(imgsize, scale=(0.2, 1.0)))
+    augs_text += 'crop,'
   if flip:
     # NOTE The original paper did not specify probability of flip, so we use the default 0.5
     augs.append(transforms.RandomHorizontalFlip())
+    augs_text += 'flip,'
   if jitter:
     augs.append(transforms.RandomApply([transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2)], p=0.8))
+    augs_text += 'jitter,'
   if grayscale:
     augs.append(transforms.RandomGrayscale(p=0.2))
+    augs_text += 'grayscale,'
   if blur:
     # NOTE The original paper may not have used PyTorch for the gaussian blur
     # NOTE The kernel size was not specified in the paper. Thus we use 3
     augs.append(transforms.GaussianBlur(3, sigma=(0.1, 2.0)))
+    augs_text += 'blur,'
 
-  return transforms.Compose(augs)
+  return transforms.Compose(augs), augs_text
 
-def validate(model, trainloader, validationloader):
+def knn_validate(model, trainloader, validationloader):
   print('KNN')
   model.eval()
   xtrain = []
@@ -66,15 +76,33 @@ def validate(model, trainloader, validationloader):
 
   model.train()
 
-def main(datasetname):
-  print("using cuda?",torch.cuda.is_available())
+  return correct/total
 
+def get_lr(optimizer):
+  for p in optimizer.param_groups:
+    return p['lr']
+
+def get_momentum(optimizer):
+  for p in optimizer.param_groups:
+    return p['momentum']
+
+def get_weight_decay(optimizer):
+  for p in optimizer.param_groups:
+    return p['weight_decay']
+
+
+def main(datasetname, runname):
+  print("using cuda?",torch.cuda.is_available())
+  n_epochs = 100
+
+  augs_text = ''
+  batch_size = 512
   if datasetname == 'CIFAR10':
-    n_epochs = 800
+    n_epochs = 100
     model = SimSiam(encoder=models.resnet18)
 
     # As described in the paper, blur wasn't used for CIFAR10 experiments
-    augmentations = get_augmentations(blur=False) 
+    augmentations, augs_text = get_augmentations(blur=False)
     transform = transforms.ToTensor() 
 
     dataset_config = {
@@ -84,7 +112,7 @@ def main(datasetname):
     }
 
     dataloader_config = {
-      'batch_size': 512,
+      'batch_size': batch_size,
       'shuffle': True,
       'num_workers': 4,
       'pin_memory': torch.cuda.is_available()
@@ -97,10 +125,24 @@ def main(datasetname):
     validationloader = torch.utils.data.DataLoader(validationset, **dataloader_config)
 
     optimizer = optim.SGD(model.parameters(), lr=0.03, momentum=0.9, weight_decay=0.0005)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=n_epochs) 
+    #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=n_epochs)
 
   if torch.cuda.is_available():
     model = model.to('cuda')
+
+  config = {
+    'dataset': datasetname,
+    'n_epochs': n_epochs,
+    'optimizer': type(optimizer),
+    'lr': get_lr(optimizer),
+    'momentum': get_momentum(optimizer),
+    'weight_decay': get_weight_decay(optimizer),
+    'batch_size': dataloader_config['batch_size'],
+    'augmentations': augs_text
+  }
+  wandb.init(project='simsiam', entity='simsiambois', save_code=True, group='simsiambois', tags=datasetname, name=runname, config=config)
+
+  wandb.watch(model)
 
   smooth_loss = 0.0
   n_iter = 0
@@ -128,11 +170,15 @@ def main(datasetname):
       lossval = loss.item()
       smooth_loss = smooth_loss*0.99 + lossval*0.01
 
+      wandb.log({'n_iter': n_iter, 'epoch': epoch, 'loss': lossval})
+
       if i % 100 == 0 and i != 0:
         print('ITER: {}, loss: {}, smooth_loss: {}'.format(n_iter, lossval, smooth_loss))
 
-    scheduler.step()
-    validate(model, trainloader, validationloader)
+    if datasetname == 'CIFAR10':
+      knn_acc = knn_validate(model, trainloader, validationloader)
+      wandb.log({'n_iter': n_iter, 'epoch': epoch, '1nn_accuracy': knn_acc})
 
 if __name__ == '__main__':
-  main('CIFAR10')
+  # TODO Maybe add argparse stuff here
+  main('CIFAR10', 'CIFAR10-testrun')
